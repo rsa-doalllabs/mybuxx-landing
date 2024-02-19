79f5c032512185e1ab65ecb01b95088e78220470
rsa-doalllabs <raymond@doalllabs.io>
2024-02-18 23:48:19 -0600

    Updated feature text

--- app/sections/BasicFeature.tsx
+++ app/sections/BasicFeature.tsx
@@ -17,7 +17,6 @@
 
 import React from 'react'
 import { MyBuxx_Iphone } from 'images/MyBuxx-Iphone'
-import { scrollToDiv } from 'utils/ClickScroll'
 
 const BasicFeature = () => {
   return (
@@ -26,9 +25,7 @@ const BasicFeature = () => {
         <div className="flex-1 order-2">
           <h2 className="text-4xl font-semibold mb-4">Feature Focus</h2>
           <p className="text-gray-500 mb-6">
-            MYBUXX HAS AN AVERAGE CUSTOMER BASE OF TWENTY-SEVEN THOUSAND MEMBERS PARTICIPATING IN THE ONLINE SERVICE.  THE MOBILE APLICATION VOLUME CURRENTLY AVERAGES FORTY THOUSAND WEEKLY, THE RESULT IN HIGH TRAFFIC AND CUSTOMER CARE IS BASED ON MYBUXX, LLC LOW FEES AND TIMELY PAYMENT DELIVERABLES
-
-            <a href="#" className="text-blue-600 hover:underline"> Page Types and Components</a>.
+            MYBUXX currently has an average customer base of twenty-seven thousand members participating in the online service. The mobile application volume currently averages forty thousand weekly, resulting in high traffic. Customer satisfaction with MYBUXX App is based on its low fees and timely payment deliverables.
           </p>
           <button className="primary-color text-white px-6 py-2 rounded-md hover:bg-blue-700 transition duration-300">
             Join the Wait List
